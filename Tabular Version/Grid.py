e664785ee22380d692b2fa31f4a659285700de58
Shreyas S R <shreyassr123@gmail.com>
2024-09-07 21:24:41 +0530

    Add files via upload

---
+++ Tabular Version/Grid.py
@@ -0,0 +1,16 @@
+from Run import *
+
+gamma   = 0.95
+noSteps = 10**5
+noRuns  = 1000
+exp     = "egreedy1divn^0.5"
+# lrs     = [ "1divn^0.8", "1divn" ]
+lrs     = [  "1divn" ]
+
+
+widths = [ 22 ]
+for lr in lrs:
+    for width in widths:
+        World   = Grid( 3, 3, width )
+        runExperiment(World, gamma, noSteps, noRuns, exp, lr)
+
